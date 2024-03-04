0ac11b8831d554711456d9a86501c8c9a8bda3b2
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-03-04 14:57:18 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -155,10 +155,10 @@ if vector_store is not None:
       st.chat_message("user").markdown(user_query)
       ret_text=search(user_query,vector_store)
       
-      system_prompt=""""""Answer the user's query in detail with the provided information.Provide a sturctured and organized output whereevr required
+      system_prompt="""Answer the user's query in detail with the provided information.Provide a sturctured and organized output whereevr required
 	             Do not generalize the answer and provide specific terms and technical content as provided in the information.
 	            Do not hallucinate,if you dont know the answer say so. The output has to in a very professional way.
-             Do not provide lengty paragraphs,instead break the output into bits of small paragraphs so that its easy to go through.The information to asnwer the query is:"""+str(ret_text).
+             Do not provide lengty paragraphs,instead break the output into bits of small paragraphs so that its easy to go through.The information to answer the query is:"""+str(ret_text).
       prompt="My query is: "+str(user_query)
       myinput = {"data":[{"prompt":prompt,"system":system_prompt}]}
     else:
