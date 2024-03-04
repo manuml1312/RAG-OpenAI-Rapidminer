cea89a86439f3113d01f0b6a86b1b0959dbdc02b
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-03-04 15:47:23 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -176,13 +176,14 @@ else:
     
 
 if st.session_state.messages[-1]["role"] != "assistant":  
-    with st.chat_message("assistant"):  
-        with st.spinner("Thinking..."): 
-            	response = requests.post(url, auth=(username, password),json=myinput)
-		response_dict = json.loads(response.text)
-            	s=response_dict['data']
-            	response2=s[0]['response']
+	
+    	with st.chat_message("assistant"):  
+        	with st.spinner("Thinking..."): 
+            		response = requests.post(url, auth=(username, password),json=myinput)
+			response_dict = json.loads(response.text)
+            		s=response_dict['data']
+            		response2=s[0]['response']
             	# response3=re.sub(re.escape("\n\n"),"",response2)
-		st.write_stream(response_generator(response2))
-           	message = {"role": "assistant", "content": response2}  
-            	st.session_state.messages.append(message)
+			st.write_stream(response_generator(response2))
+           		message = {"role": "assistant", "content": response2}  
+           		st.session_state.messages.append(message)
