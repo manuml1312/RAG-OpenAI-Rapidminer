8e195b5e5059422f4bcf83d388ec3398df7992dd
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-03-04 13:05:46 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -141,16 +141,17 @@ embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
 df=pd.read_csv('sabic_materials_data.csv')
 content=df['context'][0]
 
+
 processing(content)
 vector_store = FAISS.load_local("vector_store_faiss",embeddings) 
 if vector_store is not None:
   if user_query := st.chat_input(placeholder="Your query here"):  
       st.session_state.messages.append({"role": "user", "content": user_query})
       st.chat_message("user").markdown(user_query)
-
-  ret_text=search(user_query,vector_store)
-  prompt="Provide the citations and elucidate about "+str(user_query)+" ,from the given information. Information:"+str(ret_text)
-  myinput = {"data":[{"prompt":prompt}]}
+      ret_text=search(user_query,vector_store)
+      prompt="Provide the citations and elucidate about "+str(user_query)+" ,from the given information. Information:"+str(ret_text)
+      myinput = {"data":[{"prompt":prompt}]}
+  
 
 
 if st.session_state.messages[-1]["role"] != "assistant":  
