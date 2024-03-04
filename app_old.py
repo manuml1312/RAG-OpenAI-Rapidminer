4826905550126cf1c8c7a68c5ef49d42effab512
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-03-04 14:41:39 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -94,15 +94,24 @@ def search(user_query, vector_store):
 def response_generator(response):
     comp = response
     if '\n' in comp:
-      for line in comp.split('\n'):
-        for word in line.split():
-            yield word + " "
-            time.sleep(0.05)
-        yield "\n\n"
+        lines = comp.split('\n')
+        for i, line in enumerate(lines):
+            words = line.split()
+            for j, word in enumerate(words):
+                if j > 0:
+                    yield " "  # Add space between words
+                yield word
+                time.sleep(0.05)
+            if i < len(lines) - 1:  # Avoid adding extra newline at the end
+                yield "\n"  # Add newline between lines
     else:
-      for word in comp.split():
-          yield word + " "
-          time.sleep(0.05)
+        words = comp.split()
+        for i, word in enumerate(words):
+            if i > 0:
+                yield " "  # Add space between words
+            yield word
+            time.sleep(0.05)
+
 
 def reset_conversation():
   st.session_state.messages=st.session_state.messages = [{"role": "assistant", "content": "Mention your queries!"}]
