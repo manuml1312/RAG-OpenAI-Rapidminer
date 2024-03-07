c8b4c55e8b9d86f98ce4248da4c0b35c72512b25
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-03-07 17:23:12 +0530

    Update app_pinecone.py

--- app_pinecone.py
+++ app_pinecone.py
@@ -25,9 +25,6 @@ client=OpenAI(api_key=openai_api_key)
 
 
 pc = Pinecone(api_key=api_key)
-
-@st.cache_data
-
 index=pc.Index('genai-petro-updated')
 supporting_data=pd.read_csv('./supporting_data_petro.csv')
 
