f57bbaad23483a1a536fc2acf7f58fa67a276d68
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2024-03-11 11:52:39 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -8,7 +8,7 @@ import requests
 import json
 import time
 from PIL import Image
-
+import base64
 
 
 st.title("📝 Chatbot - SABIC Materials ") 
@@ -20,6 +20,30 @@ st.title("📝 Chatbot - SABIC Materials ")
 #     return modified_logo
 
 # st.sidebar.image(add_logo(logo_path="./logo.PNG", width=250, height=600)) 
+file = open("./logo.PNG", "rb")
+contents = file.read()
+img_str = base64.b64encode(contents).decode("utf-8")
+buffer = io.BytesIO()
+file.close()
+img_data = base64.b64decode(img_str)
+img = Image.open(io.BytesIO(img_data))
+resized_img = img.resize((150, 60))  # x, y
+resized_img.save(buffer, format="PNG")
+img_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebarNav"] {{
+                background-image: url('data:image/png;base64,{img_b64}');
+                background-repeat: no-repeat;
+                padding-top: 50px;
+                background-position: 100px 50px;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 with st.sidebar:
   st.write("""Document Name : SABIC Materials Information Retrieval Chatbot \n 
